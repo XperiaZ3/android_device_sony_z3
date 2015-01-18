5f23325106fd44aef3a950396cd946ccfb453cfe
David Vaz Guijarro <vazdavid@icloud.com>
2015-01-18 14:21:54 +0100

    Ops

--- z3.mk
+++ z3.mk
@@ -54,7 +54,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.device.rc:root/init.device.rc
     
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/init.qcom.ril.path.sh:root/init.qcom.ril.path.sh
+    $(LOCAL_PATH)/rootdir/init.qcom.ril.path.sh:root/init.qcom.ril.path.sh \
     $(LOCAL_PATH)/rootdir/init.qcom.ril.path.sh:system/etc/init.qcom.ril.path.sh
 
 # USB function switching
